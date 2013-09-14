1e104e64b11d218df44d9e1b56acbd440922021c
Pedro Rogério <pinceladasdaweb@hotmail.com>
2013-09-14 18:21:38 -0300

    Initial commit

---
+++ assets/js/carousel.js
@@ -0,0 +1,84 @@
+/*
+--------------------------------
+CSS3 Perspective Carousel
+--------------------------------
++ https://github.com/pinceladasdaweb/
++ version 1.0
++ Copyright 2013 Pedro Rogerio
++ Licensed under the MIT license
+
++ Documentation: https://github.com/pinceladasdaweb/
+*/
+var Carousel = (function ($) {
+    'use strict';
+    /*global jQuery*/
+    var module = {
+        config: function (config) {
+            this.target = config.target;
+        },
+        early: function () {
+            var self = this;
+            self.target.append('<span class="nav-left"></span><span class="nav-right"></span>');
+        },
+        moveCarousel: function ($el, direction, reverse, callback) {
+            var self = this,
+                $current = self.target.find('.is-active'),
+                indexActive;
+
+            $el.removeClass(direction);
+            $current.removeClass('is-active').addClass(direction);
+
+            indexActive = $current.siblings('.' + reverse).removeClass(reverse).addClass('is-active').index();
+            indexActive = callback(indexActive, $el) || indexActive;
+
+            if (direction === 'left') {
+                $el.eq(indexActive + 1).addClass(reverse);
+            } else {
+                $el.eq(indexActive - 1).addClass(reverse);
+            }
+        },
+        events: function () {
+            var self = this;
+
+            self.target.on('click', '.nav-left', function () {
+                self.moveCarousel($('.carousel-wrapper li'), 'right', 'left', function (cur, $el) {
+                    if ((cur - 1) <= 1) {
+                        return $el.length - 1;
+                    }
+                });
+            });
+
+            self.target.on('click', '.nav-right', function () {
+                self.moveCarousel($('.carousel-wrapper li'), 'left', 'right', function (cur, $el) {
+                    if ((cur + 1) >= ($el.length - 1)) {
+                        return '0';
+                    }
+                });
+            });
+
+            $(document).keyup(function (e) {
+                if (!self.target) {
+                    return;
+                }
+
+                if(e.which === 39) {
+                    $('.nav-right').trigger('click');
+                }
+
+                if(e.which === 37) {
+                    $('.nav-left').trigger('click');
+                }
+            });
+        },
+        init: function (config) {
+            module.config(config);
+
+            module.early();
+            module.events();
+        }
+    };
+
+    return {
+        init: module.init
+    };
+}(jQuery));
---
+++ index.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>CSS3 Perspective Carousel</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    </head>
+    <body>
+        <h1>CSS3 Perspective Carousel</h1>
+        <div class="carousel">
+            <ul class="carousel-wrapper">
+                <li class="carousel-box left">
+                    <img src="assets/img/img1.jpg" alt="CSS3 Perspective Carousel">
+                </li>
+
+                <li class="carousel-box is-active">
+                    <img src="assets/img/img2.jpg" alt="CSS3 Perspective Carousel">
+                </li>
+
+                <li class="carousel-box right">
+                    <img src="assets/img/img3.jpg" alt="CSS3 Perspective Carousel">
+                </li>
+
+                <li class="carousel-box">
+                    <img src="assets/img/img4.jpg" alt="CSS3 Perspective Carousel">
+                </li>
+
+                <li class="carousel-box">
+                    <img src="assets/img/img5.jpg" alt="CSS3 Perspective Carousel">
+                </li>
+
+                <li class="carousel-box">
+                    <img src="assets/img/img6.jpg" alt="CSS3 Perspective Carousel">
+                </li>
+
+                <li class="carousel-box">
+                    <img src="assets/img/img7.jpg" alt="CSS3 Perspective Carousel">
+                </li>
+
+                <li class="carousel-box">
+                    <img src="assets/img/img8.jpg" alt="CSS3 Perspective Carousel">
+                </li>
+
+                <li class="carousel-box">
+                    <img src="assets/img/img9.jpg" alt="CSS3 Perspective Carousel">
+                </li>
+
+                <li class="carousel-box">
+                    <img src="assets/img/img10.jpg" alt="CSS3 Perspective Carousel">
+                </li>
+
+                <li class="carousel-box">
+                    <img src="assets/img/img11.jpg" alt="CSS3 Perspective Carousel">
+                </li>
+
+                <li class="carousel-box">
+                    <img src="assets/img/img12.jpg" alt="CSS3 Perspective Carousel">
+                </li>
+            </ul>
+        </div>
+        <?php
+            $ieVersion = preg_match('/MSIE (.*?);/', $_SERVER['HTTP_USER_AGENT'], $matches) ? floatval($matches[1]) : null;
+
+            if($ieVersion < 9 && $ieVersion != null) {
+                $jQueryVersion = '1.10.2';
+            } else {
+                $jQueryVersion = '2.0.3';
+            }
+        ?>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/<?php echo $jQueryVersion; ?>/jquery.min.js"></script>
+        <script src="assets/js/carousel.js"></script>
+        <script>
+            (function() {
+                Carousel.init({
+                    target: $('.carousel')
+                });
+            })();
+        </script>
+    </body>
+</html>
